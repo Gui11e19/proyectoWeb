1ad487614d781b31f25be39949b263376a0f1d01
Guille19 <guille19@debian>
2019-11-13 17:10:38 -0600

    Conectando base de datos mongodb

--- API/app.js
+++ API/app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect('localhost:27017/store'); //investigar en que puesto esta la base
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
